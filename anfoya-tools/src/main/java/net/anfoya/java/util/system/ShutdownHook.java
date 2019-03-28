5f3cab5d3cfcbce61980c7f2f552f7f5f4390f1a
Fred A. <frederic.antigny@gmail.com>
2019-03-28 12:27:22 +0000

    handle exceptions and clean up log message

--- anfoya-tools/src/main/java/net/anfoya/java/util/system/ShutdownHook.java
+++ anfoya-tools/src/main/java/net/anfoya/java/util/system/ShutdownHook.java
@@ -3,21 +3,36 @@ package net.anfoya.java.util.system;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.anfoya.java.util.VoidCallable;
+
 public class ShutdownHook {
 	private static final Logger LOGGER = LoggerFactory.getLogger(ShutdownHook.class);
 
-	public ShutdownHook(Runnable runnable) {
-		this(runnable, true, Thread.currentThread().getStackTrace()[2].getClassName());
+	public ShutdownHook(final VoidCallable callable) {
+		this(callable, true, Thread.currentThread().getStackTrace()[2].getClassName());
 	}
 
-	public ShutdownHook(Runnable runnable, boolean daemon) {
-		this(runnable, daemon, Thread.currentThread().getStackTrace()[2].getClassName());
+	public ShutdownHook(final VoidCallable callable, final boolean daemon) {
+		this(callable, daemon, Thread.currentThread().getStackTrace()[2].getClassName());
 	}
 
-	public ShutdownHook(Runnable runnable, boolean daemon, String log) {
-		Thread hook = new Thread(() -> {
-			runnable.run();
-			LOGGER.info("[shutdown complete] {}", log);
+	protected ShutdownHook(final VoidCallable callable, final boolean daemon, final String className) {
+		final Thread hook = new Thread(() -> {
+			try {
+				callable.call();
+			} catch (Exception e) {
+				LOGGER.error("shutdown hook execution failed", e);
+				return;
+			}
+
+			String simpleName;
+			try {
+				simpleName = className.substring(className.lastIndexOf(".") + 1);
+			} catch (Exception e) {
+				simpleName = className;
+			}
+			
+			LOGGER.info("shutdown complete for {}", simpleName);
 		});
 		hook.setDaemon(daemon);
 
