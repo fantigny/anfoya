efcb8afc05db160bb9825e096cddf135237631cd
Fred A. <frederic.antigny@gmail.com>
2019-12-05 12:53:51 +0000

    add TagException constructor

--- anfoya-tag/src/main/java/net/anfoya/tag/service/TagException.java
+++ anfoya-tag/src/main/java/net/anfoya/tag/service/TagException.java
@@ -6,4 +6,8 @@ public abstract class TagException extends Exception {
 	public TagException(final String msg, final Throwable t) {
 		super(msg, t);
 	}
+
+	public TagException(String msg) {
+		super(msg);
+	}
 }
