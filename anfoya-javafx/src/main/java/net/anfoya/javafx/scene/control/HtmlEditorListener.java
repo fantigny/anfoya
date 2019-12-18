b4e301604515b64b8e50180d20f655dbf0663c4d
Fred A. <frederic.antigny@gmail.com>
2019-12-18 22:32:48 +0000

    fix enter ignored on linux

--- anfoya-javafx/src/main/java/net/anfoya/javafx/scene/control/HtmlEditorListener.java
+++ anfoya-javafx/src/main/java/net/anfoya/javafx/scene/control/HtmlEditorListener.java
@@ -2,8 +2,10 @@ package net.anfoya.javafx.scene.control;
 
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.web.HTMLEditor;
+import javafx.scene.web.WebView;
 
 public class HtmlEditorListener {
 	private final BooleanProperty editedProperty;
@@ -17,6 +19,20 @@ public class HtmlEditorListener {
 
 		editor.setOnMouseClicked(e -> checkEdition(editor.getHtmlText()));
 		editor.addEventFilter(KeyEvent.KEY_TYPED, e -> checkEdition(editor.getHtmlText()));
+		
+		// fix enter ignored on linux
+		WebView editorView = (WebView) editor.lookup(".web-view");
+		editor.addEventFilter(KeyEvent.KEY_PRESSED, e -> {
+			if (e.getCode() == KeyCode.ENTER) {
+				e.consume();
+				editorView.fireEvent(new KeyEvent(
+						e.getSource(),
+						editorView,
+						KeyEvent.KEY_TYPED,
+						"\r", "", KeyCode.ENTER,
+						false, true, false, false));
+			}
+		});
 	}
 
 	public BooleanProperty editedProperty() {
