f703a7875fef0a0d69f4f4160ab006154a4c81cd
Fred A. <frederic.antigny@gmail.com>
2019-12-19 17:14:15 +0000

    give access to win api to set app user model id

---
+++ anfoya-javafx/src/main/java/net/anfoya/javafx/util/WinShell32.java
@@ -0,0 +1,28 @@
+package net.anfoya.javafx.util;
+
+import com.sun.jna.Pointer;
+import com.sun.jna.WString;
+import com.sun.jna.platform.win32.Ole32;
+import com.sun.jna.platform.win32.Shell32;
+import com.sun.jna.ptr.PointerByReference;
+
+public class WinShell32 {
+
+	public static long setExplicitAppUserModelId(String id) {
+		return Shell32
+				.INSTANCE
+				.SetCurrentProcessExplicitAppUserModelID(new WString("FisherMail"))
+				.longValue();
+	}
+
+	public static String getCurrentProcessExplicitAppUserModelID() {
+		final PointerByReference pRef = new PointerByReference();
+		Shell32.INSTANCE.GetCurrentProcessExplicitAppUserModelID(pRef);
+
+		Pointer pointer = pRef.getValue();
+		final String currentId = pointer.getWideString(0);
+		Ole32.INSTANCE.CoTaskMemFree(pointer);
+
+		return currentId;
+	}
+}
